9f3f14f34138bdad2fa41d583d7d86a43b65350b
ewalu <ewa_lugin@tlen.pl>
2017-10-26 22:21:41 +0200

    20171026

--- src/pl/atena/edu/zadania/Plik.java
+++ src/pl/atena/edu/zadania/Plik.java
@@ -2,9 +2,12 @@ package pl.atena.edu.zadania;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 import java.util.logging.Logger;
 
 public class Plik {
@@ -12,13 +15,15 @@ public class Plik {
 	protected Logger logger = Logger.getGlobal();
 	
 	public void Wczytaj(File file) {
-		Map<String, Integer> zliczanie = new HashMap<>();
+		Map<String, Integer> zliczanie = new TreeMap<>();
 		String[] lista;
 		try {
 		Scanner sc = new Scanner(file);
 		
 		while (sc.hasNextLine()) {
 			String line = sc.nextLine();
+			line = line.toLowerCase();
+			line = line.replaceAll("[.,—„”!?]","");
 			lista = przetwarzajLinie(line);
 			zliczanie = zliczaj(zliczanie, lista);
 			
@@ -30,17 +35,18 @@ public class Plik {
 		zliczanie.entrySet().forEach(item-> System.out.println(item));
 
 	}
-//zliczanie s��w
+//zliczanie 
 public Map<String, Integer> zliczaj(Map<String, Integer> licznik, String[] lista) {
 	for (int i = 0; i<lista.length; i++) {
+		//System.out.println(lista[i]);
 		licznik.merge(lista[i], 1, Integer::sum);
 	}
 	return licznik;
 }
-//funkcja dzieli stringa na tablic� string�w
+//funkcja dzieli stringa na tablicę stringów
 public String[] przetwarzajLinie(String line) {
 	String[] lista;
-	lista = line.split("\\s");
+	lista = line.split(" ");
 	return lista;
 }
 }
